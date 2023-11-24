350d03962960899e78509969376092672df92373
pandyr12 <pandyr12@mcmaster.ca>
2023-11-24 17:58:51 -0500

    objectify gameMechs

--- GameMechs.cpp
+++ GameMechs.cpp
@@ -7,6 +7,10 @@ GameMechs::GameMechs()
 
 GameMechs::GameMechs(int boardX, int boardY)
 {
+    boardSizeX = boardX;
+    boardSizeY = boardY;
+    exitFlag = false;
+
 
 }
 
@@ -16,33 +20,34 @@ GameMechs::GameMechs(int boardX, int boardY)
 
 bool GameMechs::getExitFlagStatus()
 {
-
+    return exitFlag;
 }
 
 char GameMechs::getInput()
 {
+    return input;
 
 }
 
 int GameMechs::getBoardSizeX()
 {
-
+    return boardSizeX;
 }
 
 int GameMechs::getBoardSizeY()
 {
-
+    return boardSizeY;
 }
 
 
 void GameMechs::setExitTrue()
 {
-
+    exitFlag = true;
 }
 
 void GameMechs::setInput(char this_input)
 {
-
+    input = this_input;
 }
 
 void GameMechs::clearInput()
--- Project.cpp
+++ Project.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include "MacUILib.h"
 #include "objPos.h"
+#include "GameMechs.h"
 
 
 using namespace std;
 
 #define DELAY_CONST 100000
 
-bool exitFlag;
+GameMechs* GameMechsp;
+
+
 
 void Initialize(void);
 void GetInput(void);
@@ -23,7 +26,7 @@ int main(void)
 
     Initialize();
 
-    while(exitFlag == false)  
+    while((*GameMechsp).getExitFlagStatus() == false)  
     {
         GetInput();
         RunLogic();
@@ -41,22 +44,55 @@ void Initialize(void)
     MacUILib_init();
     MacUILib_clearScreen();
 
-    exitFlag = false;
+    GameMechsp = new GameMechs(15, 30);
 }
 
 void GetInput(void)
 {
-   
+   char input;
+   if (MacUILib_hasChar() != 0){
+	input =  MacUILib_getChar();	
+   }
+   GameMechsp->setInput(input);
 }
 
 void RunLogic(void)
 {
+    char input = GameMechsp->getInput();
+    MacUILib_printf("%c", input);
     
+    if(input != 0)  
+    {
+        switch(input)
+        {                      
+            case 27:  // exit
+                GameMechsp->setExitTrue(); 
+                break;
+            default:
+                break;
+        }
+        GameMechsp->setInput(0);
+    }
 }
 
 void DrawScreen(void)
 {
-    MacUILib_clearScreen();    
+    MacUILib_clearScreen();
+
+    for (int i = 0; i < GameMechsp->getBoardSizeX(); i++)
+    {
+        for (int j = 0; j < GameMechsp->getBoardSizeY(); j++)
+        {
+            if(i == 0 || i == GameMechsp->getBoardSizeX() - 1 || j == 0 || j == GameMechsp->getBoardSizeY() - 1){
+                MacUILib_printf("#");
+            }
+            else{
+                MacUILib_printf(" ");
+            }
+        }
+        cout << "\n";
+    }
+    
 
 }
 
@@ -68,7 +104,8 @@ void LoopDelay(void)
 
 void CleanUp(void)
 {
-    MacUILib_clearScreen();    
+    MacUILib_clearScreen();  
+    delete GameMechsp;  
   
     MacUILib_uninit();
 }
