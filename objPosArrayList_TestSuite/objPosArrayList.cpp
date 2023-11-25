91dc3bbcf7878f2b42c7f976c39f67e39b0daf8c
Tirth Nagar <nagart1@mcmaster.ca>
2023-11-24 21:31:37 -0500

    completed iter 2a

--- objPosArrayList_TestSuite/objPosArrayList.cpp
+++ objPosArrayList_TestSuite/objPosArrayList.cpp
@@ -1,54 +1,65 @@
 #include "objPosArrayList.h"
+#include "objPos.h"
 
 // Check lecture contents on general purpose array list construction, 
 // and modify it to support objPos array list construction.
 
 objPosArrayList::objPosArrayList()
 {
-
+    sizeArray = ARRAY_MAX_CAP;
+    sizeList = 0;
+    aList = new objPos[sizeArray];
 }
 
 objPosArrayList::~objPosArrayList()
 {
-
+    delete[] aList;
 }
 
 int objPosArrayList::getSize()
 {
-
+    return sizeList;
 }
 
 void objPosArrayList::insertHead(objPos thisPos)
 {
-
+    for (int i = sizeList; i > 0; i--) {
+        aList[i].setObjPos(aList[i - 1]);
+    }
+    aList[0].setObjPos(thisPos);
+    sizeList++;
 }
 
 void objPosArrayList::insertTail(objPos thisPos)
 {
-
+    aList[sizeList].setObjPos(thisPos);
+    sizeList++;
 }
 
 void objPosArrayList::removeHead()
 {
-    
+    for (int i = 0; i < sizeList - 1; ++i) {
+        aList[i].setObjPos(aList[i + 1]);
+    }
+    sizeList--;
 }
 
 void objPosArrayList::removeTail()
 {
-
+    sizeList--;
 }
 
 void objPosArrayList::getHeadElement(objPos &returnPos)
 {
-
+    returnPos.setObjPos(aList[0]);
 }
 
 void objPosArrayList::getTailElement(objPos &returnPos)
 {
-
+    returnPos.setObjPos(aList[sizeList - 1]);
 }
 
 void objPosArrayList::getElement(objPos &returnPos, int index)
 {
-
+    returnPos.setObjPos(aList[index]);
 }
